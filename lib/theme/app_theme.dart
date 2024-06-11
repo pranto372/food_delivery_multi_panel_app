ff842009a66624fb1894e002496ed737fadd4a85
pranto372 <prantodeb62@gmail.com>
2024-06-11 12:11:23 +0600

    theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,50 +3,25 @@ import 'package:food_delivery_multi_panel_app/theme/colors/app_colors.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
-<<<<<<< HEAD
     inputDecorationTheme: InputDecorationTheme(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-=======
-      inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(color: AppColors.primaryColor),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Colors.grey.shade300),
-          ),
-          hintStyle: TextStyle(
-            color: Colors.grey.shade400,
-            fontSize: 15,
-            fontWeight: FontWeight.w400,
-          ),
-          prefixIconColor: Colors.grey),
-      textTheme: TextTheme(
-        titleLarge: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w500,
-          color: Colors.black,
->>>>>>> f508c76b9b0b41d293f065a1a6037526803111c8
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: const BorderSide(color: AppColors.primaryColor),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(color: Colors.grey.shade300),
-        ),
-        hintStyle: TextStyle(
-          color: Colors.grey.shade400,
-          fontSize: 15,
-          fontWeight: FontWeight.w400,
-        ),
-        prefixIconColor: Colors.grey),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(color: AppColors.primaryColor),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(color: Colors.grey.shade300),
+      ),
+      hintStyle: TextStyle(
+        color: Colors.grey.shade400,
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+      ),
+      prefixIconColor: Colors.grey,
+    ),
     textTheme: TextTheme(
       titleLarge: const TextStyle(
         fontSize: 20,
@@ -59,14 +34,20 @@ class AppTheme {
         color: AppColors.primaryColor,
       ),
       titleSmall: TextStyle(
-          fontSize: 14,
-<<<<<<< HEAD
-          fontWeight: FontWeight.w400,
-          color: Colors.grey.shade600),
-      displayLarge: TextStyle(
-          fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: Colors.grey.shade600,
+      ),
+      displayLarge: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
       displayMedium: const TextStyle(
-          fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
@@ -75,41 +56,20 @@ class AppTheme {
         elevation: 3,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(30),
-=======
+        ),
+        textStyle: TextStyle(
           fontWeight: FontWeight.w600,
-          color: AppColors.primaryColor,
-        ),
-        titleSmall: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w400,
-            color: Colors.grey.shade600),
-        displayLarge: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black
-        ),
-        displayMedium: const TextStyle(
-            fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.primaryColor,
-          foregroundColor: Colors.white,
-          elevation: 3,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
->>>>>>> f508c76b9b0b41d293f065a1a6037526803111c8
         ),
       ),
     ),
     outlinedButtonTheme: OutlinedButtonThemeData(
       style: OutlinedButton.styleFrom(
-          elevation: 5,
-          minimumSize: Size(15, 20),
-          side: BorderSide.none,
-          backgroundColor: Colors.white,
-          shadowColor: Colors.grey.shade600),
+        elevation: 5,
+        minimumSize: Size(15, 20),
+        side: BorderSide.none,
+        backgroundColor: Colors.white,
+        shadowColor: Colors.grey.shade600,
+      ),
     ),
   );
 }
